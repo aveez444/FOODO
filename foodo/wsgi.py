68555d4502f50db89fd35f4a3ef6cb0219d86d58
aveez444 <aveezsiddiqui656@gmail.com>
2025-07-24 19:26:10 +0530

    manage

--- foodo/wsgi.py
+++ foodo/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 from django.core.wsgi import get_wsgi_application
 
